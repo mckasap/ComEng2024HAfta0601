9c5016fff424622c0610cdf585668b2e9cda98c5
mckasapbasi <mckasap@gmail.com>
2024-03-27 14:57:32 +0300

    PAŞALI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
 }
 
 dependencies {
-
+    implementation ("org.jsoup:jsoup:1.17.2")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
--- app/src/main/java/com/example/comeng2024hafta0601/MainActivity.java
+++ app/src/main/java/com/example/comeng2024hafta0601/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.comeng2024hafta0601;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.ExecutionException;
@@ -24,7 +32,11 @@ public class MainActivity extends AppCompatActivity {
         protected String doInBackground(String... strings) {
             StringBuilder sb= new StringBuilder("");
             try {
-                URL url = new URL(strings[0]);
+
+                Document doc =  Jsoup.connect(strings[0]).get();
+                Log.d("Title",doc.title());
+
+               /* URL url = new URL(strings[0]);
                 HttpsURLConnection con= (HttpsURLConnection) url.openConnection();
                 con.connect();
 
@@ -37,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
                     data= reader.read();
                 }
 
-
+            */
 
             } catch (MalformedURLException e) {
                 throw new RuntimeException(e);
@@ -49,11 +61,12 @@ public class MainActivity extends AppCompatActivity {
             return sb.toString();
         }
     }
+    ImageView iv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        iv=(ImageView) findViewById(R.id.imageView);
         myAsyncTask task= new myAsyncTask();
 
         try {
@@ -66,4 +79,43 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
+    private class GoruntuIndir extends AsyncTask<String,Void, Bitmap>{
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+
+            try {
+                URL url = new URL("http://www.agaclar.net/forum/attachments/sukulent/7290d1171654782-pasa-008.jpg");
+                HttpURLConnection con= (HttpURLConnection)url.openConnection();
+                con.connect();
+                InputStream in = con.getInputStream();
+                Bitmap btm= BitmapFactory.decodeStream(in);
+
+                return  btm;
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+    }
+
+
+    public void Getir(View V){
+
+        GoruntuIndir gi= new GoruntuIndir();
+
+        try {
+            iv.setImageBitmap(gi.execute().get());
+
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
 }
